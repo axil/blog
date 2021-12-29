38a48e486f6cb8d23adfb538378687e2e36b9b07
Lev Maximov <lev.maximov@gmail.com>
2021-12-30 05:28:36 +0700

    fix

--- !.bat
+++ !.bat
@@ -1,2 +1,3 @@
 pelican.exe
+cp C:\Documents\blog\output\a-comprehensive-guide-to-numpy-data-types.html C:\Documents\blog\output\numpy-data-types.html
 python a.py
--- a.py
+++ a.py
@@ -1,5 +1,5 @@
 import re
-s = open(r'C:\Documents\blog\output\numpy-data-types.html', encoding='utf8').read()
+s = open(r'C:\Documents\blog\output\a-comprehensive-guide-to-numpy-data-types.html', encoding='utf8').read()
 s = re.sub('<script.*?</script>', '', s, flags=re.S)
 open(r'C:\Documents\blog\output\1.html', 'w', encoding='utf8').write(s)
 
